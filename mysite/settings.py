65d018d637bce6d908506d01c831cfdd23c7ce85
WonJun Cho <cwjcsk@naver.com>
2022-08-08 14:24:06 +0900

    SQL 쿼리 콘솔창에 찍히게 하기

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,3 +147,21 @@ DEFAULT_FROM_EMAIL = f"{EMAIL_HOST_USER}@naver.com"
 
 # 로그인 후에 이동할 주소
 LOGIN_REDIRECT_URL = "/blog/"
+
+# 콘솔창에서 SQL 쿼리 보기
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
